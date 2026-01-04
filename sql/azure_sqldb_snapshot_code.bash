7f5e86f0ccbabc7e4d2d434fc2525bdaae9dc7e3
Tushar Jadhav <tusharjmech143@gmail.com>
2026-01-04 19:02:39 +0530

    Create azure_sqldb_snapshot_code.bash

---
+++ sql/azure_sqldb_snapshot_code.bash
@@ -0,0 +1,6 @@
+SqlPackage.exe /Action:Extract
+/SourceServerName:"sql-srvr-vb-dev1.database.windows.net"
+/SourceDatabaseName:"sql-db-vb-dev" 
+/SourceUser:"Adminvb" 
+/SourcePassword:"Tush@rme1" 
+/TargetFile:"C:\db-backup\sql-db-vb-dev.dacpac"
